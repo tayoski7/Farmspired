269357c1d77e19cd1e73626e07785d0e6cc7cfab
DextrousDavid <aimakhededavido@gmail.com>
2020-05-11 22:44:43 +0100

    renamed App.js to App.jsx and corrected some styles issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
