cc28665c724779be14ad0bec77230223a1b44589
DextrousDavid <aimakhededavido@gmail.com>
2020-05-11 23:26:51 +0100

    modified some App styles

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> Hello World</p>
+        <p>Trying thing out</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React Now
         </a>
