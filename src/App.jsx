2fe4fb139fa2fe7c5dddc2698f3e3c192f926705
DextrousDavid <aimakhededavido@gmail.com>
2020-05-11 23:35:53 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p> Hello World</p>
+        <p> Hello World Wow</p>
         <p>Trying thing out</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React Now
